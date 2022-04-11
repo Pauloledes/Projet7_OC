cff6ab133a5c22380a0e5aceff55fb5ca72b8462
Pauloledes <paul.ledesma@hotmail.fr>
2022-04-11 12:24:05 +0200

    add setup(*

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="P7_pledes_api",
+    version="0.1",
+    author="Gaetan Desrues",
+    author_email="paul.ledesma@hotmail.fr",
+    description="Description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pauloledes/Projet7_OC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
